b47dffb96a2309b0cb263f45a8621922882764fb
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2025-01-16 15:10:02 +0400

    added order services.

---
+++ AmazonSimulatorApp/Data/Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿
+namespace AmazonSimulatorApp.Data.Repositories
+{
+    public interface IOrderRepository
+    {
+        int AddOrder(Order order);
+        void DeleteOrder(Order order);
+        IEnumerable<Order> GetAllOrders();
+        IEnumerable<Order> GetClientOrders(int clientId);
+        Order GetOrderById(int id);
+        void UpdateOrder(Order order);
+    }
+}
