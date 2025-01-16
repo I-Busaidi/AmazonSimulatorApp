3b0a4e9e607fb5153fadb6f239cc38a05c658fb2
SalehM909 <saleh.m.alshabibi@gmail.com>
2025-01-16 13:54:25 +0400

    Add product Repo and service

---
+++ AmazonSimulatorApp/Data/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using AmazonSimulatorApp.Data;
+
+public interface IProductRepository
+{
+    Task<Product> CreateProductAsync(Product product);
+    Task<bool> DeleteProductAsync(int id);
+    Task<List<Product>> GetAllProductsAsync();
+    Task<Product> GetProductByIdAsync(int id);
+    Task<Product> UpdateProductAsync(Product product);
+    Task<ProductImages> CreateProductImagesAsync(ProductImages productImages);
+    Task<ProductImages> UpdateProductImagesAsync(ProductImages productImages);
+}
