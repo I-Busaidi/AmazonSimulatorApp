b3b8b3d87a7b8df43d84d9781764ed2af1177ad7
Alanoud <Alanoud2022it@gmail.com>
2025-01-16 15:03:50 +0400

    Add client repo , service & DTO

---
+++ AmazonSimulatorApp/Services/IClientService.cs
@@ -0,0 +1,17 @@
+﻿using AmazonSimulatorApp.Data;
+using AmazonSimulatorApp.Data.DTOs;
+
+namespace AmazonSimulatorApp.Services
+{
+    public interface IClientService
+    {
+        Client GetClientById(int clientId);
+        IEnumerable<Client> GetAllClients();
+        bool EmailExists(string email);
+        ClientOutPutDTO GetClientData(string? clientName, int? clientId);
+        void AddClient(ClientOutPutDTO input);
+        void UpdateClientDetails(updateClientDTO input);
+        void UpdateClient(Client client);
+        Client GetClientByName(string clientName);
+    }
+}
